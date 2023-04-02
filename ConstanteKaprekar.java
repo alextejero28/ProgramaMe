6eadca7bad1af557a0d179540038846996995317
alextejero28 <alextejero3@gmail.com>
2023-04-03 00:11:07 +0200

    completado

--- ConstanteKaprekar.java
+++ ConstanteKaprekar.java
@@ -1,86 +1,49 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- *
- * @author alex
- */
 public class ConstanteKaprekar {
+    public static int kaprekarIterations(int num) {
+        if (num == 6174)
+            return 0;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int iteraciones, n, numeroVueltas, nAsc, nDesc;
-                
-        iteraciones = sc.nextInt();
-        
-        String number, numberAsc = "", numberDesc = "";
-        
-        int[] resultado = new int[iteraciones];
-        int[] digits;
-        
-        for (int i = 0; i < iteraciones; i++) {
-            numeroVueltas = 0;
-            n = sc.nextInt();
+        int iterations = 0;
+        while (num != 6174) {
+            iterations++;
 
-            
-
-            while(n!=6174){
-            
-            number = String.valueOf(n);
-            
-            switch (number.length()) {
-                case 1 -> number = "000" + number;
-                case 2 -> number = "00" + number;
-                case 3 -> number = "0" + number;
-                default -> {
-                }
+            // Obtener dígitos del número y ordenarlos
+            int[] digits = new int[4];
+            for (int i = 0; i < 4; i++) {
+                digits[i] = num % 10;
+                num /= 10;
             }
-            
-            digits = new int[number.length()];
-            
-            for (int j = 0; j < number.length(); j++) {
-                 digits[j] = Character.digit(number.charAt(j), 10);                
-            }
-            
             Arrays.sort(digits);
-            
-            for (int k = 0; k < number.length(); k++) {
-                numberAsc += digits[k];
+
+            // Obtener números ascendente y descendente
+            int ascendingNum = 0;
+            int descendingNum = 0;
+            for (int i = 0; i < 4; i++) {
+                ascendingNum = ascendingNum * 10 + digits[i];
+                descendingNum = descendingNum * 10 + digits[3 - i];
             }
 
-            for (int l = number.length()- 1; l >= 0; l--) {
-                numberDesc += digits[l];
-            }
-
-            nAsc = Integer.parseInt(numberAsc);
-            nDesc = Integer.parseInt(numberDesc);
-
-            if(nAsc > nDesc){
-                n = nAsc - nDesc;
-            }else{
-                n = nDesc - nAsc;
-            }
-            
-            numeroVueltas++;
-            
-            
-            
-            
-            
-            
+            // Restar menor al mayor y actualizar el número
+            num = descendingNum - ascendingNum;
+            if (num == 0)
+                return 8; // Caso repdigits
         }
-        resultado[i] = numeroVueltas;
-        
+
+        return iterations;
     }
 
-    for(r = 0; r < iteraciones; r++){
-        System.out.println(resultado[r]);
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numCasos = scanner.nextInt();
 
-    
-}
+        for (int i = 0; i < numCasos; i++) {
+            int num = scanner.nextInt();
+            System.out.println(kaprekarIterations(num));
+        }
+
+        scanner.close();
+    }
 }
